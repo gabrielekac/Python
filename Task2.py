50ccc218afaac55342ae644d10e1e25e3b77f35d
gabrielekac <gabriele.kacinskaite@i-dental.lt>
2023-03-04 18:50:48 +0200

    Task 2

---
+++ Task2.py
@@ -0,0 +1,10 @@
+# ---------Task 2:
+# (Explore some String functions).
+# Create a program that asks the user for a password and checks if it meets the following criteria: 
+# at least 8 characters long
+# If the password meets all of these criteria, print "Password accepted : True", 
+# otherwise print "Password accepted : False".
+
+UserPassword=input("Please enter your password")
+PasswordLenght=len(UserPassword)>=8
+print("Password accepted:"+str(PasswordLenght))
